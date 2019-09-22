93a7ff687123b0bda76931a838955e1915aab13e
Nick Liu <xinliang@yit.com>
2019-09-22 21:38:29 +0800

    refine

--- start.sh
+++ start.sh
@@ -7,6 +7,7 @@ git pull
 COMMIT_HASH=$(git rev-parse HEAD)
 export IMAGE_TAG=${COMMIT_HASH:0:6}
 
+docker stop working-assistant
 ./mvnw -Dwebpack.mode=prod clean package
 
 docker build -t working-assistant:${IMAGE_TAG} .
